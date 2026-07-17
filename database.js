683ebcccc404a61dce4514a479e27c115a6e1139
estebanramos01032011-hue <esteban.ramos01032011@gmail.com>
2026-07-17 20:24:52 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,1 +1,25 @@
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
 
+let db;
+
+async function initDatabase() {
+    db = await open({
+        filename: "./bennys.db",
+        driver: sqlite3.Database
+    });
+
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS services (
+            userId TEXT PRIMARY KEY,
+            totalSeconds INTEGER DEFAULT 0
+        )
+    `);
+
+    console.log("✅ Base de données prête !");
+}
+
+module.exports = {
+    initDatabase,
+    getDb: () => db
+};
